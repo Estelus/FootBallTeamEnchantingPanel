55e1b575109960d4374cf586e6115491441572f8
Estelus <maciej.mazon@gmail.com>
2023-10-09 19:33:14 +0200

    Adding new test- Refreshing of Enchanting Panel

--- src/test/java/test_chrome_enchanting_panel.java
+++ src/test/java/test_chrome_enchanting_panel.java
@@ -58,7 +58,7 @@ public class test_chrome_enchanting_panel {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://footballteamgame.com/pl/");
-        //act - Get to the main login page. Finding drop down to log in account. Clicking on it. Provide credential to log into the web service
+        //Act - Get to the main login page. Finding drop down to log in account. Clicking on it. Provide credential to log into the web service
         driver.findElement(By.cssSelector("span.like-link.hidden")).click();
         driver.findElement(By.cssSelector("div.header")).click();
         driver.findElement(By.cssSelector("[type=email]")).sendKeys("maciej.m1993@gmail.com");
@@ -85,6 +85,23 @@ public class test_chrome_enchanting_panel {
         String url_of_enchanting_panel = driver.getCurrentUrl();
         assertEquals("https://pl.footballteamgame.com/character", url_of_enchanting_panel, "Url of enchanting panel does not match");
         driver.quit();
+    }
+    @Test
+    public void RefreshingEnchantingPanel(){
+//  1. Act - Get to the main login page. Finding drop down to log in account. Clicking on it. Provide credential to log into the web service
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://footballteamgame.com/pl/");
+        driver.findElement(By.cssSelector("span.like-link.hidden")).click();
+        driver.findElement(By.cssSelector("div.header")).click();
+        driver.findElement(By.cssSelector("[type=email]")).sendKeys("maciej.m1993@gmail.com");
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("m5rMASt8pKkChmj");
+        driver.findElement(By.cssSelector("button.isSkewed div[data-v-d2acbfea]")).click();
+//  assert - assertion to prove that the user successful logged into the website
+        String actualTitle = driver.getTitle();
+        assertEquals("Menad\u017Cer pi\u0142karski online - Pi\u0142karska gra online - graj w FootballTeam", actualTitle, "page Title does not match the expected title" );
+//  2. act - click and transfer the user to the dashboard panel
+
+
     }
 
 }
