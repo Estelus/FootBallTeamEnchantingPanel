40c218c429af37ec8285df62d55490dc7e23db6a
Estelus <maciej.mazon@gmail.com>
2023-10-31 14:27:40 +0100

    Finished refreshingEnchantingPanel test

--- src/test/java/test_Chrome_Enchanting_Panel.java
+++ src/test/java/test_Chrome_Enchanting_Panel.java
@@ -55,9 +55,8 @@ public class test_Chrome_Enchanting_Panel {
         String actualTitle = driver.getTitle();
         assertEquals("Menad\u017Cer pi\u0142karski online - Pi\u0142karska gra online - graj w FootballTeam", actualTitle, "page Title does not match the expected title" );
         //act - click and transfer the user to the dashboard panel
-        driver.findElement(By.cssSelector("div.header__menu")).click();
-        driver.findElement(By.cssSelector("div.header__menu")).click();
-        //assert - assertion to make sure the user entered the dashboard panel
+        driver.findElement(By.cssSelector("li:nth-child(1) > a > div.icons-container-wrapper > div > svg")).click();
+       //assert - assertion to make sure the user entered the dashboard panel
         String url_of_dashboard_panel = driver.getCurrentUrl();
         assertEquals("https://pl.footballteamgame.com/dashboard", url_of_dashboard_panel, "page Title does not match the expected title");
         //act - the user navigate to the character panel
@@ -74,8 +73,9 @@ public class test_Chrome_Enchanting_Panel {
 
     }
     @Test
-    public void refreshingEnchantingPanel(){
-//  1. Act - Get to the main login page. Finding drop down to log in account. Clicking on it. Provide credential to log into the web service
+    public void refreshingEnchantingPanel() throws InterruptedException {
+//   Act - Get to the main login page. Finding drop down to log in account. Clicking on it. Provide credential to log into the web service
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://footballteamgame.com/pl/");
         driver.findElement(By.cssSelector("span.like-link.hidden")).click();
         driver.findElement(By.cssSelector("div.header > button:nth-child(1)")).click();
@@ -85,11 +85,34 @@ public class test_Chrome_Enchanting_Panel {
 //  assert - assertion to prove that the user successful logged into the website
         String actualTitle = driver.getTitle();
         assertEquals("Menad\u017Cer pi\u0142karski online - Pi\u0142karska gra online - graj w FootballTeam", actualTitle, "page Title does not match the expected title" );
-//  2. act - click and transfer the user to the dashboard panel
+//      Act - click and transfer the user to the dashboard panel
+        driver.findElement(By.cssSelector("li:nth-child(1) > a > div.icons-container-wrapper > div > svg")).click();
+        //assert - assertion to make sure the user entered the dashboard panel
+        String url_of_dashboard_panel = driver.getCurrentUrl();
+        assertEquals("https://pl.footballteamgame.com/dashboard", url_of_dashboard_panel, "page Title does not match the expected title");
+        //act - the user navigate to the character panel
+        driver.findElement(By.cssSelector("body > div:nth-child(2) > div > aside > ul > li:nth-child(2) > a > div.icons-container-wrapper")).click();
+        //assert - make sure that the user changed the panel
+        String title_of_character_panel = driver.getTitle();
+        assertEquals("Dashboard | FootballTeam - Pi\u0142karska gra online", title_of_character_panel, "title of character panel does not match" );
+        //act - the user click on dropdown list box
+        driver.findElement(By.cssSelector("div > nav > ul > li:nth-child(3) > a > span")).click();
+        driver.findElement(By.cssSelector("div > nav > ul > li:nth-child(3) > ul > li:nth-child(3) > a > span > div > span")).click();
+        //assert
+        String url_of_enchanting_panel = driver.getCurrentUrl();
+        assertEquals("https://pl.footballteamgame.com/character", url_of_enchanting_panel, "Url of enchanting panel does not match");
+        driver.get("https://pl.footballteamgame.com/character/enchant");
+        driver.get("https://pl.footballteamgame.com/character/enchant");
+        driver.get("https://pl.footballteamgame.com/character/enchant");
+        driver.get("https://pl.footballteamgame.com/character/enchant");
+        driver.get("https://pl.footballteamgame.com/character/enchant");
+        //assert
+        assertEquals("https://pl.footballteamgame.com/character", url_of_enchanting_panel, "Url of enchanting panel does not match");
+
 
 //  Assertion fail because writing test case is not finished
 
-        Assertions.fail();
+     //   Assertions.fail();
 
     }
     @AfterEach
